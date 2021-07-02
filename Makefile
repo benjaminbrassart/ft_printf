c9f01c5f89a07682f62f417f7af95412ad0bd1c5
Benjamin BRASSART <bbrassar@e3r9p8.42.fr>
2021-07-02 07:53:19 +0200

    'p' conversion working

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/06/30 22:15:31 by bbrassar          #+#    #+#              #
-#    Updated: 2021/07/02 07:26:46 by bbrassar         ###   ########.fr        #
+#    Updated: 2021/07/02 07:38:37 by bbrassar         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -24,6 +24,7 @@ SRC						= $(addsuffix .c,									\
 							ft_get_printer									\
 							ft_print_char									\
 							ft_print_string									\
+							ft_print_pointer								\
 							ft_print_uint									\
 							ft_print_hex									\
 						)
--- include/print_functions.h
+++ include/print_functions.h
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/01 23:41:27 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/07/02 07:26:32 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/07/02 07:36:03 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,9 @@ int						ft_print_char(t_opt *opt, va_list args)
 int						ft_print_string(t_opt *opt, va_list args)
 						__attribute__((nonnull(1)));
 
+int						ft_print_pointer(t_opt *opt, va_list args)
+						__attribute__((nonnull(1)));
+
 int						ft_print_uint(t_opt *opt, va_list args)
 						__attribute__((nonnull(1)));
 
@@ -41,6 +44,7 @@ int						ft_print_hex_up(t_opt *opt, va_list args)
 static t_printer const	g_printers[] = {
 	{'c', ft_print_char},
 	{'s', ft_print_string},
+	{'p', ft_print_pointer},
 	{'u', ft_print_uint},
 	{'x', ft_print_hex_low},
 	{'X', ft_print_hex_up},
