5b4b82591a8a698d43422b36f95013999c64c1e3
Benjamin BRASSART <bbrassar@e3r9p8.42.fr>
2021-07-02 04:13:13 +0200

    Fixed negative star values

--- src/ft_parse_options.c
+++ src/ft_parse_options.c
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/30 20:59:25 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/07/01 23:58:07 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/07/02 04:07:09 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,6 +67,13 @@ t_opt	*ft_parse_options(char const **p_format, va_list args)
 		ft_parse_flags(p_format, opt);
 		ft_parse_min_width(p_format, args, opt);
 		ft_parse_precision(p_format, args, opt);
+		if (opt->flags & DASH)
+			opt->flags &= ~MIN_WIDTH;
+		if (opt->min_width < 0)
+		{
+			opt->flags ^= MIN_WIDTH | DASH;
+			opt->min_width *= -1;
+		}
 	}
 	return (opt);
 }
